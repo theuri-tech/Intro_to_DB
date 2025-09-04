6e1aae25ed463f8c545d47d4c699d28c8d895126
Scott Theuri <theuriscottorg@gmail.com>
2025-09-04 20:14:17 +0300

    Update script to show the full descrition of the table Books.

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,15 @@
 USE alx_book_store;
 
-SHOW FULL COLUMNS FROM Books;
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+    
