45b2cf7f4496deaf660f2e69b47bde7a632e2f65
Scott Theuri <theuriscottorg@gmail.com>
2025-09-04 15:05:32 +0300

    Update the placement of the Error code line for the checker to pass.

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from mysql.connector import Error 
+# from mysql.connector import Error 
 
 try:
     mydb = mysql.connector.connect(
@@ -13,8 +13,8 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
     
-except Error as e:
-    print(f"Failed to connect to MySQL server: {e}")
+except mysql.connector.Error as err:
+    print(f"Failed to connect to MySQL server: {err}")
 
 finally:
     try:
