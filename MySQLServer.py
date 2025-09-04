f26df250874b790bc60eebe453fcbb26d2415f80
Scott Theuri <theuriscottorg@gmail.com>
2025-09-04 15:00:20 +0300

    Add initial MySQLServer setup code

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error 
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="theuri",
+        password="1Mula",
+        database="alx_book_store"
+    )
+    if mydb.is_connected():
+        cursor = mydb.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    
+except Error as e:
+    print(f"Failed to connect to MySQL server: {e}")
+
+finally:
+    try:
+        if cursor:
+            cursor.close()
+        if mydb:
+            mydb.close()
+    except NameError:
+        pass #cursor or mydb wan't created
+
