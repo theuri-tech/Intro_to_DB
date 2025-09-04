c9358453cdc7d2a6aa8a2df964ad40e649522825
Scott Theuri <theuriscottorg@gmail.com>
2025-09-04 20:42:35 +0300

    Correct a typo in the outcome

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,6 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+SELECT * FROM customer WHERE customer_id = 1;
